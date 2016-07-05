f9f18bd37792e233266bd566ad928bc1eb5cd7cf
Sean Lee <landasiastudio@gmail.com>
2016-07-05 15:00:06 -0700

    working sample app!

--- ios/keyframes-sample-ios/keyframes-sample-ios/ViewController.m
+++ ios/keyframes-sample-ios/keyframes-sample-ios/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
+#import <keyframes/KFVector.h>
 #import <keyframes/KFVectorLayer.h>
+#import <keyframes/KFVectorParsingHelper.h>
 
 @interface ViewController ()
 
@@ -16,9 +18,34 @@
 
 @implementation ViewController
 
+- (KFVector *)loadLikeVectorFromDisk
+{
+  static KFVector *likeVector;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    NSString *filePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"sample_like" ofType:@"json" inDirectory:nil];
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    NSDictionary *likeVectorDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+    likeVector = KFVectorFromDictionary(likeVectorDictionary);
+  });
+  return likeVector;
+}
+
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  
+  KFVector *likeVector = [self loadLikeVectorFromDisk];
+  NSLog(@"faces %@", likeVector);
+  
+  KFVectorLayer *likeVectorLayer = [KFVectorLayer new];
+  const CGFloat shortSide = MIN(CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds));
+  const CGFloat longSide = MAX(CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds));
+  likeVectorLayer.frame = CGRectMake(shortSide / 4, longSide / 2 - shortSide / 4, shortSide / 2, shortSide / 2);
+  likeVectorLayer.faceModel = likeVector;
+  [self.view.layer addSublayer:likeVectorLayer];
+  [likeVectorLayer startAnimation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,3 +54,5 @@
 }
 
 @end
+
+
