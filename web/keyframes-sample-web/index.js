d1d42655b0e5a7c006256608324bb9944729f909
Thomas Aylott <aylott@fb.com>
2016-07-15 16:07:52 -0400

    A bunch of stuff
    lol, commit messages

--- web/keyframes-sample-web/index.js
+++ web/keyframes-sample-web/index.js
@@ -12,7 +12,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Surface, Group, Shape, Transform} from 'react-art';
 
-class KeyframesSample extends React.Component {
+import BezierEasing from 'bezier-easing';
+import Morph from 'art/morph/path';
+
+require('art/modes/current').setCurrent(require('art/modes/dom'));
+
 type KfDocument = {
   key: number,
   name: string,
@@ -86,87 +90,140 @@ type KfGradient = {
   ramp_start?: KfGradientStop,
   ramp_end?: KfGradientStop,
 };
+
+const defaultProps = {
+  loop: false,
+  progress: 0,
+  currentFrameNumber: 0,
+};
+
+class KfSample extends React.Component {
+  state: {
+    currentFrameNumber: number,
+  };
+
+  static defaultProps: typeof defaultProps;
+  props: {
+    doc: KfDocument,
+  } & typeof defaultProps;
+
   constructor(props, ...args) {
     super(props, ...args);
+    const {currentFrameNumber} = props;
+    this.state = {currentFrameNumber};
     this.handleRAF = this.handleRAF.bind(this);
-    console.log(props)
+    console.log(props);
   }
-  componentDidMount() {
-    this.tickRAF();
+
+  componentWillReceiveProps(nextProps) {
+    const {currentFrameNumber} = nextProps;
+    this.setState({currentFrameNumber});
   }
-  tickRAF() {
-    this._rafTimer = requestAnimationFrame(this.handleRAF);
-  }
-  componentWillUnmount() {
-    cancelAnimationFrame(this._rafTimer);
-  }
+
+  nextFrameStartTime: number;
+  _rafTimer: number;
+  handleRAF: Function;
   handleRAF() {
     const now = Date.now();
     if (!(now < this.nextFrameStartTime)) {
-      let currentFrame = (this.state || this.props).currentFrame || 0;
-      currentFrame ++;
+      let currentFrameNumber = this.state.currentFrameNumber || 0;
+      currentFrameNumber ++;
       const {frame_rate, animation_frame_count} = this.props.doc;
       const frameTime = 1000 / frame_rate
       this.nextFrameStartTime = now + frameTime;
       
-      if (currentFrame > animation_frame_count) {
+      if (currentFrameNumber > animation_frame_count) {
         if (!this.props.loop) {
           return;
         }
-        currentFrame = 0;
+        currentFrameNumber = 0;
       }
-      this.setState({currentFrame});
+      this.setState({currentFrameNumber});
     }
     this.tickRAF();
   }
+
+  tickRAF() {
+    this._rafTimer = window.requestAnimationFrame(this.handleRAF);
+  }
+  componentDidMount() {
+    this.tickRAF();
+  }
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this._rafTimer);
+  }
+
+  // blendShapesCache: {[key:string]:{}};
+  // blendShapes: (a: Path, b: Path, curve: KfTimingCurve, progress: number) => Path;
+  blendShapes(a: Array<string>, b: Array<string>, curve: KfTimingCurve, progress: number): string {
+    const aPath = Morph.Path(a.join(' '));
+    const bPath = Morph.Path(b.join(' '));
+    const tween = Morph.Tween(aPath, bPath);
+    tween.tween(progress);
+    return tween;
+  }
+
   render() {
     // console.log(this.props.doc);
-    const {features} = this.props.doc;
-    const {currentFrame} = this.state || this.props;
-    console.log({currentFrame})
+    const {
+      name,
+      canvas_size: [canvasWidth, canvasHeight],
+      features,
+    } = this.props.doc;
+    const {currentFrameNumber} = this.state;
+    // console.log({name, currentFrameNumber})
     return (
-      <div>
-        <p>Yo?!</p>
-        <Surface width={177} height={177}>
-          {features.map(({fill_color, stroke_color, stroke_width, key_frames}, index) => {
-            const shapeData = getShapeDataForCurrentFrame(key_frames, currentFrame);
-            return shapeData && (
-              <Shape key={index}
-                fill={hexColorSwapAlphaPosition(fill_color)}
-                stroke={hexColorSwapAlphaPosition(stroke_color)}
-                strokeWidth={stroke_width}
-                d={shapeData.join(' ')}
-              />
-            );
-          })}
-        </Surface>
-      </div>
+      <Surface key={name} width={canvasWidth} height={canvasHeight}>
+        {features.map((feature, index) => {
+          const {name, fill_color, stroke_color, stroke_width} = feature;
+          const shapeData = getValueForCurrentFrame(feature.key_frames, feature.timing_curves, currentFrameNumber, this.blendShapes);
+          // new ReactART.LinearGradient(["black", "white"])
+          return shapeData && (
+            <Shape key={name}
+              fill={hexColorSwapAlphaPosition(fill_color)}
+              stroke={hexColorSwapAlphaPosition(stroke_color)}
+              strokeWidth={stroke_width}
+              d={shapeData.join && shapeData.join(' ') || shapeData}
+            />
+          );
+        })}
+      </Surface>
     );
   }
 }
-KeyframesSample.defaultProps = {
-  currentFrame: 0,
-  loop: false,
-};
 
-function hexColorSwapAlphaPosition(color) {
+KfSample.defaultProps = defaultProps;
+
+
+// function convertKfDocToNestedLayers(kfDoc: KfDocument): KfArtTree {
+//
+// }
+
+
+function hexColorSwapAlphaPosition(color: ?string): ?string {
   if (!color) {
     return;
   }
   return '#' + color.substr(3,6) + color.substr(1,2);
 }
 
-
-function getShapeDataForCurrentFrame(key_frames, targetFrame): ?Array<string> {
-  let kFrame;
-  if (key_frames.length > 0) {
-    let _kFrame, targetFrameBestMatch = -1;
-    for (let index = key_frames.length; --index >= 0;){
-      _kFrame = key_frames[index];
-      const {start_frame} = _kFrame;
+function getValueForCurrentFrame<T>(
+  kfValues: Array<KfValue<T>>,
+  timing_curves?: Array<KfTimingCurve>,
+  targetFrame: number,
+  blend?: (a:T, b:T) => T,
+): ?T {
+  let kfValueIndex, kfValue, kfValueNext;
+  if (kfValues.length > 0) {
+    let _kfValue, targetFrameBestMatch = -1;
+    for (let index = -1, kfValuesCount = kfValues.length; ++index < kfValuesCount;) {
+      _kfValue = kfValues[index]
+      const {start_frame} = _kfValue;
       // There can't be more than one perfect match
       if (start_frame === targetFrame) {
-        kFrame = _kFrame;
+        kfValue = _kfValue;
+        kfValueIndex = index;
+        kfValueNext = null;
         break;
       }
       // Skip any that happen later than now
@@ -176,35 +233,57 @@ function getShapeDataForCurrentFrame(key_frames, targetFrame): ?Array<string> {
       // Keep any that are closer to the target
       if (start_frame >= targetFrameBestMatch) {
         targetFrameBestMatch = start_frame;
-        kFrame = _kFrame;
+        kfValue = _kfValue;
+        kfValueIndex = index;
+        kfValueNext = kfValues[index + 1];
       }
     }
   }
-  return kFrame && kFrame.data || null;
+  if (!kfValue) {
+    kfValueIndex = 0;
+    kfValue = kfValues[kfValueIndex];
+    kfValueNext = kfValues[kfValueIndex + 1];
+  }
+  if (kfValue && kfValueNext && timing_curves && blend && kfValueIndex != null) {
+    const minFrame = kfValue.start_frame;
+    const maxFrame = kfValueNext.start_frame;
+    const progressBetweenFrames = (targetFrame - minFrame) / (maxFrame - minFrame);
+    const curve = timing_curves[kfValueIndex];
+    return blend(kfValue.data, kfValueNext.data, curve, progressBetweenFrames);
+  }
+  return kfValue && kfValue.data || null;
 }
 
 
-// function filterStartFrameToThis(({start_frame}), index, key_frames): boolean {
-//   const targetFrame = this;
-//   const previousFrame = key_frames[index - 1];
-//   const previousStartFrame = previousFrame && previousFrame.start_frame || NaN;
-//   const nextFrame = key_frames[index + 1];
-//   const nextStartFrame = nextFrame && nextFrame.start_frame || NaN;
-//
-//   if (nextStartFrame <= targetFrame) {
-//     return false;
-//   }
-//   return true;
-// }
+class KfDemo extends React.Component {
+  state: {currentFrameNumber:number};
+  constructor(...args) {
+    super(...args);
+    this.state = {currentFrameNumber:0};
+  }
+  render() {
+    const {currentFrameNumber} = this.state;
+    return (
+      <div>
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_anger_temp.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/anger.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sorry.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_haha.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_like.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/yay.json')} />
+        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/love.json')} />
+        <br />
+        <input type="range" defaultValue={currentFrameNumber} min={0} max={100} step={1} onChange={
+          ({target:{valueAsNumber:currentFrameNumber}}) => this.setState({currentFrameNumber})
+        } />
+      </div>
+    );
+  }
+}
 
-
-document.write('<div id=KeyframesSampleRoot></div>');
+document.write('<div id=KfSampleRoot></div>');
 
 ReactDOM.render(
-  <div>
-    <KeyframesSample doc={require('./assets/sample_anger_temp.json')} />
-    <KeyframesSample doc={require('./assets/sample_haha.json')} />
-    <KeyframesSample doc={require('./assets/sample_like.json')} />
-  </div>,
-  document.getElementById('KeyframesSampleRoot')
+  <KfDemo />,
+  document.getElementById('KfSampleRoot')
 );
