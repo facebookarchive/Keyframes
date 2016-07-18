293162e7604646741aef39c789796233e19cefc7
Thomas Aylott <aylott@fb.com>
2016-07-18 12:42:20 -0400

    [web] Support for animation groups

--- web/keyframes-sample-web/index.js
+++ web/keyframes-sample-web/index.js
@@ -97,7 +97,7 @@ const defaultProps = {
   currentFrameNumber: 0,
 };
 
-class KfSample extends React.Component {
+class KfImage extends React.Component {
   state: {
     currentFrameNumber: number,
   };
@@ -155,16 +155,28 @@ class KfSample extends React.Component {
 
   // blendShapesCache: {[key:string]:{}};
   // blendShapes: (a: Path, b: Path, curve: KfTimingCurve, progress: number) => Path;
-  blendShapes(a: Array<string>, b: Array<string>, curve: KfTimingCurve, progress: number): string {
+  blendShapes(a: Array<string>, b: Array<string>, curve: KfTimingCurve, progress: number): any {
+    // TODO(aylott): Cache BezierEasing objects instead of recreating them constantly
+    const [[curveA, curveB], [curveC, curveD]] = curve;
+    const easing = BezierEasing(curveA, curveB, curveC, curveD);
     // TODO(aylott): Cache Path and Tween objects instead of recreating them constantly
     const aPath = Morph.Path(a.join(' '));
     const bPath = Morph.Path(b.join(' '));
     const tween = Morph.Tween(aPath, bPath);
+    tween.tween(easing(progress));
+    return tween;
+  }
+  blendNumbers(aNums: Array<number>, bNums: Array<number>, curve: KfTimingCurve, progress: number): Array<number> {
     // TODO(aylott): Cache BezierEasing objects instead of recreating them constantly
     const [[curveA, curveB], [curveC, curveD]] = curve;
     const easing = BezierEasing(curveA, curveB, curveC, curveD);
-    tween.tween(easing(progress));
-    return tween;
+    const easedProgress = easing(progress);
+    const blendedNums = new Array(aNums.length);
+    for (let index = aNums.length; --index >= 0;){
+      blendedNums[index] = blendNumbersLinear(aNums[index], bNums[index], easedProgress);
+    }
+    // if (blendedNums[1] < 0) debugger;
+    return blendedNums;
   }
 
   render() {
@@ -179,30 +191,150 @@ class KfSample extends React.Component {
     return (
       <Surface key={name} width={canvasWidth} height={canvasHeight}>
         {features.map((feature, index) => {
-          const {name, fill_color, stroke_color, stroke_width} = feature;
-          const shapeData = getValueForCurrentFrame(feature.key_frames, feature.timing_curves, currentFrameNumber, this.blendShapes);
+          const {name, fill_color, stroke_color, feature_animations, key_frames, timing_curves, } = feature;
+          const shapeData = getValueForCurrentFrame(key_frames, timing_curves, currentFrameNumber, this.blendShapes);
+          let {stroke_width} = feature;
+          feature_animations && feature_animations.filter(filterToStroke).forEach(({property, timing_curves, key_values}) => {
+            const values: Array<number> = getValueForCurrentFrame(key_values, timing_curves, currentFrameNumber, this.blendNumbers);
+            stroke_width = values[0];
+          });
+          
           // new ReactART.LinearGradient(["black", "white"])
-          return shapeData && (
+          let transform = transformFromAnimations(feature_animations, currentFrameNumber, this.blendNumbers);
+          if (feature.animation_group) {
+            const groupTransform = transformUsingAnimationGroups(this.props.doc, feature.animation_group, currentFrameNumber, this.blendNumbers);
+            if (transform) {
+              transform = groupTransform.transform(transform);
+            } else {
+              transform = groupTransform;
+            }
+          }
+          
+          const shapeElement = shapeData && (
             <Shape key={name}
               fill={hexColorSwapAlphaPosition(fill_color)}
               stroke={hexColorSwapAlphaPosition(stroke_color)}
               strokeWidth={stroke_width}
               d={shapeData.join && shapeData.join(' ') || shapeData}
+              transform={transform}
             />
           );
+          
+          return shapeElement;
         })}
       </Surface>
     );
   }
 }
 
-KfSample.defaultProps = defaultProps;
+KfImage.defaultProps = defaultProps;
+
+const filterToStroke = ({property}) => property === 'STROKE_WIDTH';
+
+function blendNumbersLinear(aNum: number, bNum: number, progress: number): number {
+  const delta = bNum - aNum;
+  return delta * progress + aNum;
+}
+
+function testEqual(testEqualFn, expected){
+  const result = testEqualFn();
+  if (!(result === expected)) {
+    console.error({testEqualFn, result, expected});
+  }
+}
+
+testEqual(() => blendNumbersLinear(0, 1, 0), 0)
+testEqual(() => blendNumbersLinear(0, 1, 1), 1)
+testEqual(() => blendNumbersLinear(0, 1, 0.5), 0.5)
+testEqual(() => blendNumbersLinear(-99, 99, 0.5), 0)
+testEqual(() => blendNumbersLinear(-99, 99, 0.75), 49.5)
 
 
 // function convertKfDocToNestedLayers(kfDoc: KfDocument): KfArtTree {
 //
 // }
 
+function filterGroupsByThisId({group_id}: KfAnimationGroup): boolean {
+  const targetID: number = this;
+  return group_id === targetID;
+}
+
+function transformUsingAnimationGroups(
+  doc: KfDocument,
+  id: number,
+  currentFrameNumber: number,
+  blend?: (a:T, b:T, curve: KfTimingCurve, progress: number) => T,
+): Transform {
+  const group = doc.animation_groups.filter(filterGroupsByThisId, id)[0];
+  if (!group) {
+    throw new Error(`Animation Group ${id} not found`);
+  }
+  const {animations, parent_group} = group;
+  let transform = transformFromAnimations(animations, currentFrameNumber, blend);
+  if (parent_group) {
+    const groupTransform = transformUsingAnimationGroups(doc, parent_group, currentFrameNumber, blend);
+    transform = groupTransform.transform(transform);
+  }
+  return transform;
+}
+
+
+function transformFromAnimations<T>(
+  animations: Array<KfProperty>,
+  currentFrameNumber: number,
+  blend?: (a:T, b:T, curve: KfTimingCurve, progress: number) => T,
+): ?Transform {
+  if (!(animations && animations.length > 0)) {
+    return;
+  }
+  const transform = new Transform();
+  animations.sort(sortAnimations).forEach(({property, anchor, timing_curves, key_values}) => {
+    const values: Array<number> = getValueForCurrentFrame(key_values, timing_curves, currentFrameNumber, blend);
+    switch (property) {
+    case 'STROKE_WIDTH':
+      break;
+    case 'POSITION':
+      if (anchor) {
+        transform.translate(-anchor[0], -anchor[1]);
+      } else {
+        const defaultAnchor = key_values[0].data;
+        transform.translate(-defaultAnchor[0], -defaultAnchor[1]);
+      }
+      transform.translate(values[0], values[1]);
+      // anchor && transform.translate(anchor[0], anchor[1]);
+      break;
+    case 'SCALE':
+      anchor && transform.translate(anchor[0], anchor[1]);
+      transform.scale(values[0] / 100, values[1] / 100);
+      anchor && transform.translate(-anchor[0], -anchor[1]);
+      break;
+    case 'ROTATION':
+      transform.rotate(values[0], anchor[0], anchor[1]);
+      break;
+    default:
+      console.warn('Skipping unsupported property', property);
+    }
+  });
+  return transform;
+}
+
+const AnimationOrder = [
+  'STROKE_WIDTH',
+  'POSITION',
+  'SCALE',
+  'ROTATION',
+];
+
+function sortAnimations({property:aProp}: KfProperty, {property:bProp}: KfProperty): number {
+  const a = AnimationOrder.indexOf(aProp);
+  const b = AnimationOrder.indexOf(bProp);
+  return a > b
+    ? 1
+    : b > a
+      ? -1
+      : 0
+  ;
+}
 
 function hexColorSwapAlphaPosition(color: ?string): ?string {
   if (!color) {
@@ -215,7 +347,7 @@ function getValueForCurrentFrame<T>(
   kfValues: Array<KfValue<T>>,
   timing_curves?: Array<KfTimingCurve>,
   targetFrame: number,
-  blend?: (a:T, b:T) => T,
+  blend?: (a:T, b:T, curve: KfTimingCurve, progress: number) => T,
 ): ?T {
   let kfValueIndex, kfValue, kfValueNext;
   if (kfValues.length > 0) {
@@ -246,7 +378,6 @@ function getValueForCurrentFrame<T>(
   if (!kfValue) {
     kfValueIndex = 0;
     kfValue = kfValues[kfValueIndex];
-    kfValueNext = kfValues[kfValueIndex + 1];
   }
   if (kfValue && kfValueNext && timing_curves && blend && kfValueIndex != null) {
     const minFrame = kfValue.start_frame;
@@ -269,25 +400,24 @@ class KfDemo extends React.Component {
     const {currentFrameNumber} = this.state;
     return (
       <div>
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_anger_temp.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/anger.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sorry.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_haha.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_like.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/yay.json')} />
-        <KfSample currentFrameNumber={currentFrameNumber} doc={require('./assets/love.json')} />
-        <br />
-        <input type="range" defaultValue={currentFrameNumber} min={0} max={100} step={1} onChange={
+        <input style={{width:'100%'}} type="range" defaultValue={currentFrameNumber} min={0} max={100} step={0.01} onChange={
           ({target:{valueAsNumber:currentFrameNumber}}) => this.setState({currentFrameNumber})
         } />
+        <br />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/sorry.json')} />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/anger.json')} />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/haha.json')} />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/sample_like.json')} />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/yay.json')} />
+        <KfImage currentFrameNumber={currentFrameNumber} doc={require('./assets/love.json')} />
       </div>
     );
   }
 }
 
-document.write('<div id=KfSampleRoot></div>');
+document.write('<div id=KfImageRoot></div>');
 
 ReactDOM.render(
   <KfDemo />,
-  document.getElementById('KfSampleRoot')
+  document.getElementById('KfImageRoot')
 );
