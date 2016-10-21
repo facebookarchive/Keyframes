6d0e3b1980e8925dfac603772cd67393c6aedeae
Mark Peng <markpeng@fb.com>
2016-10-21 14:49:47 -0700

    update samples license
    include a LICENSE-sample.md at the root of each sample app, and change
    headers to reference this

--- android/keyframes-sample/src/main/java/com/facebook/keyframes/sample/MainActivity.java
+++ android/keyframes-sample/src/main/java/com/facebook/keyframes/sample/MainActivity.java
@@ -1,13 +1,10 @@
-/* This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only.  Facebook reserves all rights not expressly granted.
+/* Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * This source code is licensed under the license found in the
+ * LICENSE-sample file in the root directory of this source tree.
  */
+
 package com.facebook.keyframes.sample;
 
 import android.Manifest;
