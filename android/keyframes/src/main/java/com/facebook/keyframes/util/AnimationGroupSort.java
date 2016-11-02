58e9066ed0c79431ac50e870fea913e2bf5cd1d0
lozzle <mahkuh@gmail.com>
2016-11-02 14:30:17 -0700

    add license header

--- android/keyframes/src/main/java/com/facebook/keyframes/util/AnimationGroupSort.java
+++ android/keyframes/src/main/java/com/facebook/keyframes/util/AnimationGroupSort.java
@@ -1,3 +1,10 @@
+/* Copyright (c) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
 package com.facebook.keyframes.util;
 
 import com.facebook.keyframes.model.KFAnimationGroup;
