db3995f3cfe81e233934a5a84bd6a57622f25400
Rohan Dhruva <rdhruva@fb.com>
2016-11-22 17:21:18 -0800

    Update the licence for the project to an open source BSD licence

--- ios/keyframes/src/DataModel/KFVectorAnimationGroup.h
+++ ios/keyframes/src/DataModel/KFVectorAnimationGroup.h
@@ -1,11 +1,11 @@
 /**
- * Copyright (c) 2016, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the LICENSE file in
- * the root directory of this source tree.
- *
- */
+* Copyright (c) 2016-present, Facebook, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
 /**
  * This file is generated using the remodel generation script.
  * The name of the input file is KFVectorAnimationGroup.value
@@ -14,12 +14,12 @@
 #import <Foundation/Foundation.h>
 
 /**
- * Copyright (c) 2016, Facebook, Inc.
+ * Copyright (c) 2016-present, Facebook, Inc.
  * All rights reserved.
  * 
- * This source code is licensed under the license found in the LICENSE file in
- * the root directory of this source tree.
- * 
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 @interface KFVectorAnimationGroup : NSObject <NSCopying, NSCoding>
 
--- ios/keyframes/src/DataModel/remodel-plugins/fb-copyright-header-comment.js
+++ ios/keyframes/src/DataModel/remodel-plugins/fb-copyright-header-comment.js
@@ -1,6 +1,6 @@
 "use strict";
 var HEADER_COMMENTS = [
-    { content: '/**\n * Copyright (c) 2016, Facebook, Inc.\n * All rights reserved.\n *\n * This source code is licensed under the license found in the LICENSE file in\n * the root directory of this source tree.\n *\n */' }
+    { content: '/**\n* Copyright (c) 2016-present, Facebook, Inc.\n* All rights reserved.\n*\n* This source code is licensed under the BSD-style license found in the\n* LICENSE file in the root directory of this source tree. An additional grant\n* of patent rights can be found in the PATENTS file in the same directory.\n*/' }
 ];
 function createPlugin() {
     return {
