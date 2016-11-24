fa02047a5d3254d2839369037ec09c34dfee8174
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-23 22:56:21 -0600

    Add swift demo

---
+++ ios/keyframes-sample-ios/swift/keyframes-sample-ios/keyframes-sample-iosTests/keyframes_sample_iosTests.swift
@@ -0,0 +1,25 @@
+//
+//  keyframes_sample_iosTests.swift
+//  keyframes-sample-iosTests
+//
+//  Created by Hans Knoechel on 23/11/2016.
+//  Copyright © 2016 Appcelerator. All rights reserved.
+//
+
+import XCTest
+@testable import keyframes_sample_ios
+
+class keyframes_sample_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // Leave here for future unit-tests
+}
