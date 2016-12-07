3b64a175ddf99b35790a354c5722af0eba2fb0c0
Roy Marmelstein <marmelroy@gmail.com>
2016-12-07 07:17:29 +0100

    Fix unnecessary code in header and typo in podspec

--- ios/keyframes/src/Keyframes.h
+++ ios/keyframes/src/Keyframes.h
@@ -6,17 +6,8 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-/**
- * This file is generated using the remodel generation script.
- * The name of the input file is KFVector.value
- */
-
-#ifndef Keyframes_H
-#define Keyframes_H
 
 #import "KFVector.h"
 #import "KFVectorParsingHelper.h"
 #import "KFVectorLayer.h"
 #import "KFUtilities.h"
-
-#endif /* Keyframes_H */
