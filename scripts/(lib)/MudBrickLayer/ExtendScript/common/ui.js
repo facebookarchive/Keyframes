ca8a7bad4771d2d189e29098e32c0e136cc1e564
Thomas Aylott <aylott@fb.com>
2017-01-19 08:10:59 -0800

    error handling
    Summary: Better error handling for event handlers
    
    Reviewed By: lozzle
    
    Differential Revision: D4398478
    
    fbshipit-source-id: 8b3dc7735771d029590746f36565abd374567d2f

--- scripts/(lib)/MudBrickLayer/ExtendScript/common/ui.js
+++ scripts/(lib)/MudBrickLayer/ExtendScript/common/ui.js
@@ -5,6 +5,8 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * Run this script in Adobe After Effects
  */
 var UI = exports;
 
@@ -44,12 +46,17 @@ function setProps(props){
   if (typeof props.render === 'function') {
     props = props.render.call(view, props);
   }
-  for (propName in props) {
+  var prop;
+  for (var propName in props) {
     if (propName in SetPropsBlacklist) {
       // skip Object.prototype properties and blacklisted properties
       continue;
     }
-    view[propName] = props[propName];
+    prop = props[propName];
+    if (typeof prop == 'function') {
+      prop = _wrapFn(prop);
+    }
+    view[propName] = prop;
   }
   if (Array.isArray(props.children)) {
     addChildren({
@@ -75,4 +82,41 @@ function addChild(props){
   var view = props.parent.add(props.type || DEFAULT_TYPE, props.bounds, props.text, props.properties);
   view.setProps = setProps;
   return view.setProps(props);
+}
+
+var _onError = function(error){
+  console.error(error);
+  alert(
+    '(' + error.fileName +
+    ':' + error.line +
+    ':' + error.start +
+    '-' + error.end +
+    ') ' + error.message +
+    '\n\n' +
+    $.stack
+  );
+};
+
+UI.setOnError = function(onError){
+  if (typeof onError !== 'function') {
+    throw Error('Invalid onError function');
+  }
+  _onError = onError;
+}
+
+function _wrapFn(fn){
+  return function _wrapFnCaller(){
+    try {
+      var results = fn.apply(this, Array.prototype.slice.call(arguments));
+    } catch(e) {
+      if (_onError) {
+        try {
+          _onError(e);
+        } catch(e1) {
+          alert(e1.toString());
+        }
+      }
+      throw e;
+    }
+  }
 }
