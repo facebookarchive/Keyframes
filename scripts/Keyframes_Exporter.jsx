a71a3fbec562e91f7aca51e9bac9c90042294154
Thomas Aylott <aylott@fb.com>
2017-01-19 08:11:01 -0800

    Previewer UI
    Summary: Added the Previewer to the UI and awesomized a bunch of stuff
    
    Reviewed By: lozzle
    
    Differential Revision: D4398505
    
    fbshipit-source-id: b16805f729310e043cb7bf464a04f2f7d6bb0d45

--- scripts/Keyframes_Exporter.jsx
+++ scripts/Keyframes_Exporter.jsx
@@ -12,73 +12,112 @@ var __dirname = File($.fileName).parent.fsName;
 
 $.level = 0; // disable debugging
 
-// JS alternative to #include "./lib/MudBrickLayer/index.jsxinc"
-$.evalFile(File(__dirname + '/lib/MudBrickLayer/ExtendScript/AE/index-ae.jsxinc'));
+// JS alternative to #include "./(lib)/MudBrickLayer/index.jsxinc"
+$.evalFile(File(__dirname + '/(lib)/MudBrickLayer/ExtendScript/AE/index-ae.jsxinc'));
 require.wasIncludedFrom($.fileName); // make relative requires work
 
 ;(function(){
 
-var AECompToKeyframesAnimation = require('./lib/keyframes/AECompToKeyframesAnimation');
-var UI = require('./lib/MudBrickLayer/ExtendScript/common/ui');
+var AECompToKeyframesAnimation = require('./(lib)/keyframes/AECompToKeyframesAnimation');
+var UI = require('./(lib)/MudBrickLayer/ExtendScript/common/ui');
+var KeyframesPreviewer = require('./(lib)/KeyframesPreviewer');
 
 // Set this to true if you want to VERY VERY SLOWLY export a "keyframeTweens"
 // array with the computed values of every property that has keyframes
 VirtualTween.ENABLED = false;
 
 function main(){
+  if ($.global.KeyframesPreviewerPanel) {
+    try{$.global.KeyframesPreviewerPanel.close();}catch(e){}
+  }
+
+  var previousTime = Date.now();
   AppWindow({
-    title: 'FB Keyframes',
-    onExport: exportActiveComp,
     log: function(message){
-      exportActiveComp.logFile && exportActiveComp.logFile.write(new Date().toISOString() + '\t' + message);
+      var  timeStamp = new Date();
+      var duration = timeStamp.getTime() - previousTime;
+      if (exportActiveComp.logFile) {
+        try {
+          exportActiveComp.logFile.write(timeStamp.toISOString() + '\t(' + duration + ')\t' + message);
+        } catch(e){
+          exportActiveComp.logFile.write(timeStamp.toString() + '\t(' + duration + ')\t' + message);
+        }
+      } else {
+        $.level > 0 && $.writeln(message);
+      }
+      previousTime = timeStamp.getTime();
     }
   });
 }
 
-function AppWindow(app){
-  function Ref(id){ return function(view){ refs[id] = view; }; }
+function AppWindow(appProps){
   var refs = {};
-
-  function exportButtonOnClick(){
-    refs.exportButton.onClick = function(){};
-    refs.exportButton.enabled = false;
-    refs.status.text = 'Exporting';
-    var start = Date.now();
-
-    app.onExport();
-
-    refs.status.text = 'Done. ' + Math.round((Date.now() - start) / 100) / 10 + ' seconds';
-    refs.exportButton.enabled = true;
-    refs.exportButton.onClick = exportButtonOnClick;
+  function Ref(id){ return function(view){ refs[id] = view; }; }
+  function setStatusText(message) {
+    if (refs.status) {
+      refs.status.text = message.toString();
+    }
   }
 
   UI.render(
-    {ref:Ref('win'), type:'palette', title:app.title, alignChildren:'fill', children:[
+    {ref:Ref('win'), type:'palette', title:'Keyframes Exporter + Previewer', alignChildren:'fill', children:[
 
       {orientation:'row', children:[
-        {ref:Ref('exportButton'), type:'button', text:'Export', onClick:exportButtonOnClick},
-        {ref:Ref('status'), type:'statictext', text:'Ready', characters:40},
+        {ref:Ref('exportButton'), type:'button', text:'Export', onClick:function(){
+          setStatusText('Exporting');
+          var start = Date.now();
+          var results = exportActiveComp({shouldPreview:true});
+          setStatusText('Done. ' + Math.round((Date.now() - start) / 100) / 10 + ' seconds');
+          // alert(results);
+        }},
+
+        {ref:Ref('status'), type:'edittext', properties:{readonly:true}, text:'Ready', characters:40},
+
+        {type:'button', text:'Help', onClick:function(){ File(__dirname).execute(); }},
+      ]},
+
+      {orientation:'row', children:[
+        {type:'button', text:'Resend', onClick:function(){
+          var kfFile = File(File(getCompRawJSONFile().fsName).fsName.replace('.json', '.kf.json'));
+          setStatusText(kfFile.toString());
+          alert(KeyframesPreviewer.previewJSONAtPath(kfFile.fsName));
+        }},
+
+        {type:'button', text:'Open Export Logs', onClick:function(){
+          var logFile = File(File(getCompRawJSONFile().fsName).fsName.replace('.json', '.log'));
+          setStatusText(logFile.toString());
+          logFile.execute();
+        }},
+
+        {type:'button', text:'Open Folder', onClick:function(){ getCompRawJSONFile().parent.execute(); }},
+
+        {ref:Ref('installButton'), type:'button', text:'Install Android App', onClick:function(){
+          setStatusText('Installing...');
+          var results = KeyframesPreviewer.install();
+          alert(results);
+          setStatusText('Installed');
+        }},
       ]},
 
     ]}
   );
 
+  $.global.KeyframesPreviewerPanel = refs.win;
+
   refs.win.show();
 
-  function writeToWindowLog(text) {
-    refs.status.text = text;
-    app.log && app.log(text);
-  }
   console.configure({
-    stdout:{write: writeToWindowLog},
-    stderr:{write: writeToWindowLog},
+    stdout:{write: function(text){
+      appProps.log && appProps.log(text);
+      setStatusText(text);
+    }},
+    stderr:{write: function(text){
+      appProps.log && appProps.log(text);
+    }},
   });
 }
 
-function exportActiveComp() {
-  // Enable verbose logging in MudBrickLayer so we can show progress
-  $.global.MudBrickLayer.debug = console.log.bind(console);
-
+function getCompRawJSONFile(){
   var activeComp = app.project && app.project.activeItem;
   if (!activeComp) {
     alert('Failed to get the activeItem. No comp selected? Select a comp and try again');
@@ -88,36 +127,64 @@ function exportActiveComp() {
     alert('Failed to convert the selected item');
   }
   var cleanName = activeItemObject.name.replace(/[^a-z0-9 _-]/ig, '');
-  var filePathRaw = app.project.file.fsName + '.comp-' + activeItemObject.id + '-' + cleanName + '.json';
+  var projectFile = app.project.file;
+  if (projectFile == null) {
+    throw Error('Project file does not exist. Save your ptoject and try again.');
+  }
+  var filePathRaw = projectFile.fsName + '.comp-' + activeItemObject.id + '-' + cleanName + '.json';
+  return File(filePathRaw);
+}
+
+function exportActiveComp(props) {
+  // Enable verbose logging in MudBrickLayer so we can show progress
+  $.global.MudBrickLayer.debug = console.log.bind(console);
+  // $.global.MudBrickLayer.debug = null; // DEBUG
+
+  var rawExportFile = getCompRawJSONFile();
+  var filePathRaw = rawExportFile.fsName;
   var filePathKf = filePathRaw.replace('.json', '.kf.json');
   var filePathLog = filePathRaw.replace('.json', '.log');
 
   exportActiveComp.logFile = File(filePathLog);
   exportActiveComp.logFile.open('w');
-  
-  console.log('Exporting "' + cleanName + '" from "' + app.project.file.fsName + '"');
+
+  console.log('Exporting "' + app.project.activeItem.name + '" from "' + app.project.file.fsName + '"');
+
+  console.time('toJSON export');
+  var activeItemJSONObject = app.project.activeItem.toJSON();
+  console.timeEnd('toJSON export');
 
   console.log('Encoding...');
-  var activeItemJSON = JSON.stringify(activeItemObject, null, 2);
+  console.time('stringify export');
+  var activeItemJSON = JSON.stringify(activeItemJSONObject, null, 2);
+  console.timeEnd('stringify export');
 
   // Make sure we're dealing with basic JS objects
   // instead of fancy ExtendScript class instances
   console.log('Cloning...');
+  console.time('clone export');
   var activeItemObjectClone = eval('(' + activeItemJSON + ')');
+  console.timeEnd('clone export');
 
   console.log('Exporting...');
+  console.time('AECompToKeyframesAnimation');
   var keyframesDocument = AECompToKeyframesAnimation(activeItemObjectClone);
+  console.timeEnd('AECompToKeyframesAnimation');
+
+  // Quick fix to work around expected int value
+  keyframesDocument.frame_rate = Math.round(keyframesDocument.frame_rate);
 
   console.log('Saving...');
+  console.time('saving');
   fs_writeFileSync(filePathKf, JSON.stringify(keyframesDocument, null, 2));
+  console.timeEnd('saving');
   console.log('FB Keyframes JSON Exported:');
   console.log(filePathKf);
 
-  if (Hooks.afterCreation.isSupported()) {
-    Hooks.afterCreation.fire(filePathKf);
-  } else {
-    // TODO: Figure out how to preview on Windows
-    File(filePathKf).parent.execute();
+  var results;
+  if (props && props.shouldPreview) {
+    results = KeyframesPreviewer.previewJSONAtPath(filePathKf);
+    console.log(results);
   }
 
   fs_writeFileSync(filePathRaw, activeItemJSON);
@@ -126,33 +193,17 @@ function exportActiveComp() {
 
   $.global.MudBrickLayer.debug = undefined; // Disable verbose logging
   exportActiveComp.logFile.close();
+  
+  return results;
 }
-
-var Hooks = {
-  afterCreation: {
-    isSupported: function(){
-      return $.os.indexOf('Macintosh OS') === 0;
-    },
-    _file: File(__dirname + '/lib/hooks-macos/after-creation.sh'),
-    fire: function(exportedFilePath){
-      if (!this._file.exists) {
-        console.error('After Creation Hook disabled. Enable by creating "' + this._file.fsName + '"');
-        return;
-      }
-      var exportedFile = File(exportedFilePath);
-      if (!exportedFile.exists) {
-        console.error('After Creation Hook NOT called because the exported file does not exist: "' + exportedFile.fsName + '"');
-        return;
-      }
-      var command = '"' + this._file.fsName + '" "' + exportedFile.fsName + '"';
-      console.log('Calling After Creation Hook command: `' + command + '`');
-      console.log(system.callSystem(command));
-    }
-  }
-};
 
 ////////////////////////////////////////////////////////////////////////////////
 
-main();
+try {
+  main();
+} catch(e){
+  alert(e.toString());
+  throw e;
+}
 
 }());
