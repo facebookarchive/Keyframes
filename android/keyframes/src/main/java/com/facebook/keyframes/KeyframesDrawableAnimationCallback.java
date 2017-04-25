8927d878763e51f3868f6226573c1b3f14ddfbc3
Renyu Liu <sd.qd.lry@gmail.com>
2017-04-24 18:15:54 -0700

    hold a strong reference of the listener in case it got garbage collected
    Summary: Closes https://github.com/facebookincubator/Keyframes/pull/129
    
    Differential Revision: D4944075
    
    Pulled By: LazyChild
    
    fbshipit-source-id: bd599d0891d91e835a9e101b74de23f807e8b2b6

--- android/keyframes/src/main/java/com/facebook/keyframes/KeyframesDrawableAnimationCallback.java
+++ android/keyframes/src/main/java/com/facebook/keyframes/KeyframesDrawableAnimationCallback.java
@@ -159,7 +159,9 @@ public abstract class KeyframesDrawableAnimationCallback {
   }
 
   protected void advanceAnimation(final long frameTimeMillis) {
-    if (mListener.get() == null) {
+    // hold a strong reference to the listener to prevent getting a null during this method.
+    FrameListener listener = mListener.get();
+    if (listener == null) {
       cancelCallback();
       mStartTimeMillis = 0;
       mPreviousProgressMillis = 0;
@@ -177,7 +179,7 @@ public abstract class KeyframesDrawableAnimationCallback {
     int currentLoopNumber = (int) (frameTimeMillis - mStartTimeMillis) / mMillisPerLoop;
     final boolean loopHasEnded = currentLoopNumber > mCurrentLoopNumber;
     if (mStopAtLoopEnd && loopHasEnded) {
-      mListener.get().onProgressUpdate(mFrameCount);
+      listener.onProgressUpdate(mFrameCount);
       stop();
       return;
     }
@@ -191,7 +193,7 @@ public abstract class KeyframesDrawableAnimationCallback {
     }
 
     if (shouldUpdateProgress) {
-      mListener.get().onProgressUpdate((float) currentProgressMillis / mMillisPerLoop * mFrameCount);
+      listener.onProgressUpdate((float) currentProgressMillis / mMillisPerLoop * mFrameCount);
     }
     mCurrentLoopNumber = (int) (frameTimeMillis - mStartTimeMillis) / mMillisPerLoop;
     if (!mStopped) {
